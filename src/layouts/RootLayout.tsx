0e40b6b056db46434533c4ba8c0387cd615c5adc
Habeeb Kareem <olaidekareem43@yahoo.com>
2023-01-18 21:22:24 +0100

    Setup up RootLayout component

--- src/layouts/RootLayout.tsx
+++ src/layouts/RootLayout.tsx
@@ -1,9 +1,22 @@
+import {Grid, GridItem} from '@chakra-ui/react'
 import {Outlet} from 'react-router-dom'
+import Navbar from '../components/Navbar'
 
 export default function RootLayout() {
   return (
-    <div>
-      <Outlet />
-    </div>
+    <Grid templateColumns="repeat(6, 1fr)">
+      <GridItem
+        as="aside"
+        colSpan={1}
+        bg="purple.400"
+        minHeight="100vh"
+        p="30px">
+        <span>Sidebar</span>
+      </GridItem>
+      <GridItem as="main" colSpan={5} p="40px">
+        <Navbar />
+        <Outlet />
+      </GridItem>
+    </Grid>
   )
 }
